b55d06053da136e327450296bc3019abe4606cfa
lhg0502 <neworigin@ccc.local>
2015-11-19 15:01:45 +0800

    Hello.lua

---
+++ HelloLua.lua
@@ -0,0 +1,12 @@
+--
+-- Author: neworigin
+-- Date: 2015-11-19 09:24:35
+--
+myName = "beauty girl"
+
+HelloTable = {name = "mutou",IQ = 125}
+function HelloAdd(num1,num2)
+	return (num1 + num2 )
+end
+local num = ccp_Number(10,20)
+print(num)
